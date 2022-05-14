bd432ddb7f10f7610fc8ab34bcff9904be8e6efb
Maksim Kuznetsov <maxekb@live.ru>
2022-05-14 12:31:38 +0300

    Добавьте файлы проекта.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.colorPannel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.Location = new System.Drawing.Point(12, 12);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(300, 300);
+            this.canvasPanel.TabIndex = 0;
+            // 
+            // colorPannel
+            // 
+            this.colorPannel.Location = new System.Drawing.Point(12, 318);
+            this.colorPannel.Name = "colorPannel";
+            this.colorPannel.Size = new System.Drawing.Size(300, 50);
+            this.colorPannel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 384);
+            this.Controls.Add(this.colorPannel);
+            this.Controls.Add(this.canvasPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvasPanel;
+        private System.Windows.Forms.Panel colorPannel;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        bool down = false;
+        const int canvasSize = 30;
+        PictureBox[,] canvas = new PictureBox[canvasSize, canvasSize];
+        PictureBox[] colors = new PictureBox[10];
+        Image selectedImage = Properties.Resources._0;
+
+        //PictureBox[] pic1 = new PictureBox[10];
+        public Form1()
+        {
+            InitializeComponent();
+
+            int size = 7;
+            for (int i = 0; i < canvasSize; i++)
+            {
+                for (int j = 0; j < canvasSize; j++)
+                {
+                    canvas[i, j] = new PictureBox();
+                    canvas[i, j].Parent = canvasPanel;
+                    canvas[i, j].Image = selectedImage;
+                    canvas[i, j].Size = new Size(size, size);
+                    canvas[i, j].Location = new Point(i * size, j * size);
+                    canvas[i, j].MouseMove += CanvasClick;
+                    canvas[i, j].Click += FormMouseDown;
+                    //canvas[i, j].MouseUp += FormMouseUp;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                colors[i] = new PictureBox();
+                colors[i].Parent = colorPannel;
+                colors[i].Size = new Size(20, 20);
+                colors[i].Location = new Point(i * 20, 0);
+                colors[i].Click += ColorsClick;
+                switch (i)
+                {
+                    case 0:
+                        colors[i].Image = Properties.Resources._0;
+                        break;
+                    case 1:
+                        colors[i].Image = Properties.Resources._1;
+                        break;
+                    case 2:
+                        colors[i].Image = Properties.Resources._2;
+                        break;
+                    case 3:
+                        colors[i].Image = Properties.Resources._3;
+                        break;
+                    case 4:
+                        colors[i].Image = Properties.Resources._4;
+                        break;
+                    case 5:
+                        colors[i].Image = Properties.Resources._5;
+                        break;
+                    case 6:
+                        colors[i].Image = Properties.Resources._6;
+                        break;
+                    case 7:
+                        colors[i].Image = Properties.Resources._7;
+                        break;
+                    case 8:
+                        colors[i].Image = Properties.Resources._8;
+                        break;
+                    case 9:
+                        colors[i].Image = Properties.Resources._9;
+                        break;
+                }
+            }
+        }
+        public void FormMouseDown(Object sender, EventArgs e)
+        {
+            down ^= true;
+        }
+        public void FormMouseUp(Object sender, EventArgs e)
+        {
+            down = false;
+        }
+        public void CanvasClick(Object sender, EventArgs e)
+        {
+            if (down)
+            {
+                PictureBox pixel = sender as PictureBox;
+                pixel.Image = selectedImage;
+            }
+        }
+        public void ColorsClick(Object sender, EventArgs e)
+        {
+            PictureBox color = sender as PictureBox;
+            selectedImage = color.Image;
+        }
+    }
+}
